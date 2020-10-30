f089a9e8843a6b7914bad742f61c868221ab4196
hm-factor <hsmanel@gmail.com>
2020-10-30 00:53:47 -0400

    general skeleton

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import './app.css';
 function App() {
   return (
     <div className="App">
-      <h1>Question</h1>
-      <button>answer</button>
-      <button>answer</button>
-      <button>answer</button>
-      <button>answer</button>
+      <h1 className="question-container">How does this work?</h1>
+      <div className="answers-container">
+        <button className="answer-component">It does</button>
+        <button className="answer-component">It doesn't</button>
+        <button className="answer-component">Work?</button>
+        <button className="answer-component">Work.</button>
+      </div>
     </div>
   );
 }
