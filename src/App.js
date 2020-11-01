736ed73630aa2a4f5f0107080f33c09131dda4e4
hm-factor <hsmanel@gmail.com>
2020-11-01 17:06:40 -0500

    set up rand questions w hooks

--- src/App.js
+++ src/App.js
@@ -1,17 +1,50 @@
 import './app.css';
+import data from './data.json';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <h1 className="question-item">How does this work?</h1>
-      <div className="answers-grid">
-        <button className="answer-item">It does</button>
-        <button className="answer-item">It doesn't</button>
-        <button className="answer-item">Work?</button>
-        <button className="answer-item">Work.</button>
+  const [questions, setQuestions] = useState([]);
+  const [begin, setBegin] = useState(false);
+
+  const tenRandQuestions = [];
+
+  useEffect(() => {
+    console.log(questions)
+  }, [questions]);
+
+  function selectQuestions() {
+    while (tenRandQuestions.length < data.length) {
+      let rand = Math.floor(Math.random() * Math.floor(data.length));
+
+      if (!tenRandQuestions.includes(rand)) tenRandQuestions.push(rand);
+    }
+  }
+  
+  function handleBegin() {
+    selectQuestions();
+    setQuestions(tenRandQuestions);
+    setBegin(true);
+  };
+
+  if (begin) {
+    return (
+      <div className="App">
+        <h1 className="question-item">{data[0].question}</h1>
+        <div className="answers-grid">
+          <button className="answer-item">{data[0].correct}</button>
+          <button className="answer-item">{data[0].incorrect[0]}</button>
+          <button className="answer-item">{data[0].incorrect[1]}</button>
+          <button className="answer-item">{data[0].incorrect[2]}</button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="App">
+        <button className="begin" onClick={handleBegin}>Begin Quiz</button>
+      </div>
+    )
+  }
 }
 
 export default App;
