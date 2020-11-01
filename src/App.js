033ec637c4bb46b74b6a89feca9c86166ca6db97
hm-factor <hsmanel@gmail.com>
2020-11-01 15:39:35 -0500

    added json

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import './app.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="question-container">How does this work?</h1>
-      <div className="answers-container">
-        <button className="answer-component">It does</button>
-        <button className="answer-component">It doesn't</button>
-        <button className="answer-component">Work?</button>
-        <button className="answer-component">Work.</button>
+      <h1 className="question-item">How does this work?</h1>
+      <div className="answers-grid">
+        <button className="answer-item">It does</button>
+        <button className="answer-item">It doesn't</button>
+        <button className="answer-item">Work?</button>
+        <button className="answer-item">Work.</button>
       </div>
     </div>
   );
